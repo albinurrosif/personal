31356ea839bd426bdef0df156a9566ebaad6a903
albi nur <albi27076@gmail.com>
2025-10-10 17:03:50 +0700

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
-  theme: { extend: {} },
+  theme: { extend: { screens: { '2xl': '1536px' } } },
   plugins: [],
 };
